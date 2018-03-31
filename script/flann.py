566a7c1608bb94acb7b2b7d72ad10995a33ef615
will <shell_tdf@126.com>
2018-03-31 23:43:22 +0800

    add new script

---
+++ script/flann.py
@@ -0,0 +1,27 @@
+
+from _common import *
+
+def getDependency( str_name ,getDependency):
+    list_name = []
+    
+    return list_name + [str_name]
+    
+    
+def SBI( str_name , b_only_download ,dict_config, getLibrary ):
+    # print(str_name)
+    
+    download_source(str_name,"https://github.com/mariusmuja/flann.git")
+    if(b_only_download):
+        return
+        
+    # STR_CGG = ''
+    # if(dict_config['static']):
+        # STR_CGG += ''
+    # else:
+        # STR_CGG += ''
+    
+    configure(str_name,dict_config)
+    build(str_name,dict_config)
+    install(str_name,dict_config)
+    
+    
